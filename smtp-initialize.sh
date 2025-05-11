20f62bdcf518532ca52a5a3f96d8499fa6582625
mrkimoz <75150244+mrkimoz@users.noreply.github.com>
2025-05-11 03:32:08 +0300

    Update and rename smtp.sh to smtp-initialize.sh

---
+++ smtp-initialize.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script will Start Postal as SMTP and Worker Servers Only.
+# Created By : Eng. Mohamed Kamal
+# Email      : Mr.peacock2@gmail.com
+# Phone      : +201062008120
+
+# Bootstraping Postal
+postal bootstrap mi.trustoutboxment.com
+cd /opt/postal/config/
+wget https://raw.githubusercontent.com/mrkimoz/postal/refs/heads/main/SMTP-postal.yml
+awk '/^rails:/ {print; in_rails=1; next} in_rails && /^  / {print; next} in_rails {exit}' /opt/postal/config/postal.yml >> SMTP-postal.yml
+mv /opt/postal/config/postal.yml /opt/postal/config/postal.yml.original
+cp -r SMTP-postal.yml postal.yml
+mv /opt/postal/install/docker-compose.yml /opt/postal/install/docker-compose.yml.old
+cd /opt/postal/install/
+wget https://raw.githubusercontent.com/mrkimoz/postal/refs/heads/main/SMTP-Compose.yml
+cp -r SMTP-Compose.yml docker-compose.yml
+postal start
