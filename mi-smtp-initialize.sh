be032ec2df8ae1076da3ad0ef1c836d6d10d892f
mrkimoz <75150244+mrkimoz@users.noreply.github.com>
2025-05-29 17:30:16 +0300

    Create mi-smtp-initialize.sh

---
+++ mi-smtp-initialize.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This script will Start Postal as SMTP and Worker Servers Only.
+# Created By : Eng. Mohamed Kamal
+# Email      : Mr.peacock2@gmail.com
+# Phone      : +201062008120
+
+
+# Install Postal Helper
+postal stop
+rm -rf /opt/postal
+rm -rf /usr/bin/postal
+git clone https://github.com/postalserver/install /opt/postal/install
+sudo ln -s /opt/postal/install/bin/postal /usr/bin/postal
+
+# Bootstraping Postal
+postal bootstrap app.obmengine.com
+cd /opt/postal/config/
+wget https://raw.githubusercontent.com/mrkimoz/postal/refs/heads/main/SMTP-postal.yml
+mv /opt/postal/config/postal.yml /opt/postal/config/postal.yml.original
+cp -r SMTP-postal.yml postal.yml
+mv /opt/postal/install/docker-compose.yml /opt/postal/install/docker-compose.yml.old
+cd /opt/postal/install/
+wget https://raw.githubusercontent.com/mrkimoz/postal/refs/heads/main/SMTP-Compose.yml
+cp -r SMTP-Compose.yml docker-compose.yml
+postal start
